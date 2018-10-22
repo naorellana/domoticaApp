3ca038400412a787eb859d752233407f9d737381
naorellana <1005alexis@gmail.com>
2018-10-22 01:46:33 -0600

    primera version de alarma lista para entrega, pendiente sensor de magnetismo o switch

--- arduinoCode/ejemplos/sistemaAlarma/sistemaAlarma.ino
+++ arduinoCode/ejemplos/sistemaAlarma/sistemaAlarma.ino
@@ -8,14 +8,19 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 String a="1 ACTIIVAR";
 String b="2 DESACTIVAR";
 String secret="3190";   //contraseña
-int contador=2;
+int contador=0;
+int intentos=1;
+boolean estado=true;
 
+const int buzzer = 12; //buzzer to arduino pin 5d
+byte sensorpir = 3;
+byte led=2;
 
 #include<Keypad.h>
 const byte filas = 4;
 const byte columnas = 4;
-byte pinesF[filas] = {11,10,9,8};
-byte pinesC[columnas] = {7,6,5,4};
+byte pinesF[filas] = {11,10,9,8};//cables de izquierda a derecha
+byte pinesC[columnas] = {7,6,5,4};//continuacion cables de izquierda a derecha
  String texto="";
 char teclas[filas][columnas] = {
  
@@ -31,13 +36,25 @@ char tecla;
  
 void setup() {
   imprimirPantalla(a,b); 
+  pinMode(led, OUTPUT);
+  pinMode(sensorpir, INPUT);
 }
 void loop() {
-  if (contador<3){
-    
+
+  //activa led de alerta o buzzer si estado es true y hay movimiento en sensores
+  if(digitalRead(sensorpir) == HIGH && estado==true){
+    digitalWrite(led, HIGH);
+    sonido(1);
   }
   else{
-    imprimirPantalla("BLOQUEDO","BLOQUEDO");
+    digitalWrite(led, LOW);
+  }
+
+  //bloquea cuando se ha llegado al limite de intento
+  if (contador<intentos){
+  }
+  else{
+    imprimirPantalla("BLOQUEADO","BLOQUEADO");
     delay(5000);
     imprimirPantalla(a,b);
     contador=0;
@@ -61,6 +78,8 @@ void loop() {
       }
       if (clave==secret){
         imprimirPantalla("SISTEMA ACTIVADO","PRES 2, DESACTIVAR");
+        //monitorear
+        estado=true;
         contador=0;
         delay(2000);
         imprimirPantalla(a,b);
@@ -68,7 +87,7 @@ void loop() {
       else{
         imprimirPantalla("ERROR","INTENTE DE NUEVO");
         contador++;
-        delay(2000);
+        delay(1000);
         imprimirPantalla("INTENTO FALLIDO","PRES 1 pra activar");
       }
   }
@@ -76,7 +95,7 @@ void loop() {
       int i=0;
       String clave="";
       String mascaraTexto="";
-      imprimirPantalla("INGRESE","CONTRASENA");
+      imprimirPantalla("INGRESE CLAVE","DESACTIVANDO");
       while (i<4){
         if(tecla = teclado.getKey()){
           i++;
@@ -86,14 +105,18 @@ void loop() {
         }
       }
       if (clave==secret){
-        imprimirPantalla("SISTEMA DESACTIVADO","PRES 1 para activar");
+        imprimirPantalla("SISTEMA DESACTIVADO","PRES 1, activar");
+        //monitorear
+        estado=false;
+        contador=0;
         delay(2000);
         imprimirPantalla(a,b);
       }
       else{
         imprimirPantalla("ERROR","INTENTE DE NUEVO");
-        delay(2000);
-        imprimirPantalla(a,b);
+        contador++;
+        delay(1000);
+        imprimirPantalla("INTENTO FALLIDO","PRES 1 pra desactivar");
       }
   }
 }
@@ -124,3 +147,30 @@ void imprimirPantalla(String t1, String t2){
   delay(1000);
 }
 
+void sensorMov(){
+  
+  if(digitalRead(sensorpir) == HIGH)
+  {
+    //dispLCD("Detectado Ingreso","Sensor Movimiento");
+    Serial.println("Detectado Ingreso Sensor Movimiento");
+    Serial.println("Detectado Ingreso Sensor Movimiento");
+    sonido(1);
+  delay(250);
+  sonido(1);
+  //delay(250);
+  //sonido(1);
+  }
+  
+}
+
+
+//********** FUNCION DE TONO (BUZZER) ******************
+void sonido(int segundos){
+   
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(segundos*1000);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  //delay(1000);        // ...for 1sec
+}
+//******************************
